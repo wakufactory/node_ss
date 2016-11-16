ba52e8746aa996684e2b3728bb2a4cbee710e05e
wakufactory <w-github@wakufactory.jp>
2016-11-16 15:05:27 +0900

    update server/main

--- server/main.js
+++ server/main.js
@@ -1,10 +1,11 @@
 "use strict";
 var server = require('./server.js') ;
 var api = require('./api_s.js') ;
-
+var ip = "127.0.0.1" ;
+if(process.argv.length>1) ip = process.argv[2] ;
 //start server
 server.start({
-	"address":"127.0.0.1",
+	"address":ip,
 	"port":8080,
 	"docroot":"../client/",
 	"api_callback":api.process,
