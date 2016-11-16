251b3af095938b161b6e55fc286b04f2ab1ba9f6
wakufactory <w-github@wakufactory.jp>
2016-11-16 14:46:15 +0900

    update electron client

--- electron/main.js
+++ electron/main.js
@@ -3,14 +3,9 @@
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
+var ipc = electron.ipcMain ;
 
 var mainWindow = null;
-
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
-    app.quit();
-});
-
 app.on('ready', function() {
 	// ブラウザ(Chromium)の起動, 初期画面のロード
 	mainWindow = new BrowserWindow({width: 650, height: 550,"allowpopups":"true"});
@@ -25,3 +20,15 @@ app.on('ready', function() {
 		mainWindow = null;
 	});
 });
+
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+ipc.on('requestMessage',function(ev,args){
+	if(args.cmd=="open_window") {
+		var subWindow = new BrowserWindow({width: 650, height: 550,"allowpopups":"false"});
+		subWindow.loadURL(args.url);
+		ev.sender.send('resoinseMessage',"ok") ;
+	}
+})
